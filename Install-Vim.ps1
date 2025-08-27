5cef9bab8a9fca6be414571f835717661ad0ce02
Tim <143897232+tr314@users.noreply.github.com>
2025-08-27 14:37:34 -0400

    Create Install-Vim.ps1

---
+++ Install-Vim.ps1
@@ -0,0 +1,17 @@
+<#
+Install and configure Vim.
+*>
+
+cd ~
+
+Write-Host "Installing _vimrc."
+Copy-Item linux-config/.vimrc ./_vimrc -Force
+
+Write-Host "Installing vim plugins."
+mkdir .\vimfiles
+Copy-Item linux-config\bundle ./vimfiles -Recurse -Force
+Copy-Item linux-config\Ultisnips ./vimfiles -Recurse -Force
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/vimfiles/bundle/Vundle.vim
+
+Write-Host "Installation complete."
