563fb182c6a7e2f1ca7fff099ab080a65cc87d6b
Tim <143897232+tr314@users.noreply.github.com>
2025-08-27 12:07:24 -0400

    Update install-config-files.sh

--- install-config-files.sh
+++ install-config-files.sh
@@ -10,3 +10,5 @@ cp linux-config/_vimrc ./.vimrc
 mkdir vimfiles
 cp -r linux-config/bundle ./vimfiles
 cp -r linux-config/Ultisnips ./vimfiles
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
