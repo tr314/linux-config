55edd338f93ed2bc5d027cca5c49a4571e5045f2
Tim <143897232+tr314@users.noreply.github.com>
2025-08-27 12:04:39 -0400

    Update install-config-files.sh

--- install-config-files.sh
+++ install-config-files.sh
@@ -8,5 +8,5 @@ cp linux-config/.bash_aliases .
 cp linux-config/_vimrc ./.vimrc
 
 mkdir vimfiles
-cp linux-config/bundle ./vimfiles
-cp linux-config/Ultisnips ./vimfiles
+cp -r linux-config/bundle ./vimfiles
+cp -r linux-config/Ultisnips ./vimfiles
