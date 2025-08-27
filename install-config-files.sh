d5d56d458664d7a7da1ec6a807ab6e7f77a1d3da
Tim <143897232+tr314@users.noreply.github.com>
2025-08-27 14:29:23 -0400

    Added console messages.

--- install-config-files.sh
+++ install-config-files.sh
@@ -4,9 +4,13 @@
 # ====================================================
 
 cd ~
+echo "Installing bash aliases."
 cp linux-config/.bash_aliases .
+
+echo "Installing vimrc."
 cp linux-config/_vimrc ./.vimrc
 
+echo "Installing vim plugins."
 mkdir vimfiles
 cp -r linux-config/bundle ./vimfiles
 cp -r linux-config/Ultisnips ./vimfiles
@@ -14,4 +18,3 @@ cp -r linux-config/Ultisnips ./vimfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "Installation complete."
-
